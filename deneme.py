5814c0fc1cccc0eccba9107992f4d9625432a4ac
alperen <130922662+coder-alperen@users.noreply.github.com>
2023-05-18 20:17:46 +0300

    Add files via upload

---
+++ deneme.py
@@ -0,0 +1,38 @@
+import discord
+import os
+import random
+import requests
+from discord.ext import commands
+from settings import *
+
+
+
+img_files=os.listdir("C:/Users/Casper/Desktop/code/kodland_python/denemeler/images")
+intents = discord.Intents.default()
+intents.message_content = True
+
+def get_woof_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+bot = commands.Bot(command_prefix='*', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command('dog')
+async def dog(ctx):
+    image_url = get_woof_image_url()
+    await ctx.send(image_url)
+
+@bot.command()
+async def mem(ctx):
+    img_file=random.choice(img_files)
+    with open(f'C:/Users/Casper/Desktop/code/kodland_python/denemeler/images/{img_file}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+bot.run(settings["TOKEN"])
